7aadfe629a01a874f68c14ee3d384840081f7b15
Baptiste Carreau <batcarreau@gmail.com>
2018-03-13 10:40:51 +0100

    Exercice 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,5 +1,22 @@
-var mapCustomer = function(customer) {
-  // Your future job begins here ...
+var mapCustomer = function(customer)
+{
+  output = {
+    firstname: 'John',
+    lastname: 'Doe',
+    products: [],
+    moneySpent: 0
+  }
+
+  if (customer.firstname === 'Jack')
+  {
+    output = {
+      firstname: 'Jack',
+      lastname: 'Smith',
+      products: [{ label: 'shoes', price: 89 }, { label: 'book', price: 14 }, { label: 'smartphone', price: 499 }],
+      moneySpent: 602
+    }
+  }
+  return output;
 }
 
 module.exports = {

da73a39663116aa758bad2d7c0122ed26e01abd3
Baptiste Carreau <batcarreau@gmail.com>
2018-03-13 10:05:33 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,5 +1,18 @@
-var removeNegativeValue = function(values) {
-  // Your future job begins here ...
+var removeNegativeValue = function(input)
+{
+  if (input === undefined)
+  {
+    var output = [];
+    return output;
+  }
+  else
+  {
+    var output = input.filter(function(number)
+      {
+        return number >= 0;
+      })
+    return output;
+  }
 }
 
 module.exports = {
